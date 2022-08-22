6ea4b7facefd6943c39c3db9429fbc23bad0dbd0
Alan Maxwell <alanmaxwellantunes@gmail.com>
2022-08-21 23:35:09 -0300

    feat: implements start command on main file

---
+++ cmd/go_message_cli/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/alanmxll/go_message_cli/commands"
+	"github.com/alanmxll/go_message_cli/internal"
+)
+
+var commandList = []internal.Command{
+	new(commands.Start),
+}
+
+func main() {
+	err := internal.CommandInit("go_message_cli").Start(commandList)
+
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+}
